2fabce69b7befce2fc01d1bcd2eb91674037d311
PetryE98 <eric.petry@mail.schwarz>
2023-04-24 23:45:25 +0200

    d

--- logs.py
+++ logs.py
@@ -1,8 +1,8 @@
 import pygtail
-import asyncio
+import asyncio"
 import discord
 
-token = "MTEwMDE2MjIyNjYyMDQ3MzQ4NA.G1vSmM.8oCkJU-KkgetM7BYXzBiAUtEBapwVOSwtkxgi4"
+token = "
 channel_id = 1100162737100836947  # Remove quotes to use an integer ID
 log_file = "/home/eric/minecraft/logs/latest.log"
 
